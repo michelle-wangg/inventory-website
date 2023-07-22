913d07df335c17c5c4574bd2e234169258362440
Michelle W <michelle@dhcp-128-189-233-92.ubcsecure.wireless.ubc.ca>
2023-07-22 12:42:51 -0700

    add backend link to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { useEffect, useState } from "react";
 import Home from './components/Home';
 import About from './components/About';
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("https://inventory-website-cqbt.onrender.com")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  },[]);
   return (
     <Router>
       <div className="App">
@@ -13,6 +21,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
+        <h1>{message}</h1>
       </div>
     </Router>
   );
