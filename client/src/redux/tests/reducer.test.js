a6c766a61c327bb57e0ab13bbeb49187cdaf20bf
Michelle W <michelle@Michelles-MacBook-Pro.local>
2023-07-23 11:53:42 -0700

    add tests for reducer

---
+++ client/src/redux/tests/reducer.test.js
@@ -0,0 +1,171 @@
+// reducer.test.js
+
+import itemsReducer from '../items/reducer';
+import { addItemAsync, getItemsAsync, deleteItemAsync, addUnitAsync } from '../items/thunks';
+import { REQUEST_STATE } from '../utils'; // Assuming you have defined REQUEST_STATE
+
+describe('items reducer', () => {
+  const initialState = {
+    list: [],
+    getItems: REQUEST_STATE.IDLE,
+    addItem: REQUEST_STATE.IDLE,
+    deleteItem: REQUEST_STATE.IDLE,
+    addUnit: REQUEST_STATE.IDLE,
+    error: null,
+  };
+
+  it('should handle initial state', () => {
+    expect(itemsReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('should handle getItemsAsync.pending', () => {
+    expect(
+      itemsReducer(initialState, getItemsAsync.pending())
+    ).toEqual({
+      ...initialState,
+      getItems: REQUEST_STATE.PENDING,
+      error: null,
+    });
+  });
+
+  it('should handle getItemsAsync.fulfilled', () => {
+    const fetchedItems = [{ _id: 'item-id-1', name: 'Item 1', description: 'Description 1', price: 10 }];
+    expect(
+      itemsReducer(initialState, getItemsAsync.fulfilled(fetchedItems))
+    ).toEqual({
+      ...initialState,
+      getItems: REQUEST_STATE.FULFILLED,
+      list: fetchedItems,
+    });
+  });
+
+  it('should handle getItemsAsync.rejected', () => {
+    const error = new Error('Error adding units');
+    const rejectedAction = getItemsAsync.rejected(error);
+    expect(
+      itemsReducer(initialState, rejectedAction)
+    ).toEqual({
+      ...initialState,
+      getItems: REQUEST_STATE.REJECTED,
+      error: rejectedAction.error, // Updated to use the error object from the action
+    });
+  });
+
+  it('should handle addItemAsync.pending', () => {
+    expect(
+      itemsReducer(initialState, addItemAsync.pending())
+    ).toEqual({
+      ...initialState,
+      addItem: REQUEST_STATE.PENDING,
+      error: null,
+    });
+  });
+
+  it('should handle addItemAsync.fulfilled', () => {
+    const itemToAdd = { _id: 'item-id-123', name: 'New Item', description: 'Description', price: 10 };
+    expect(
+      itemsReducer(initialState, addItemAsync.fulfilled(itemToAdd))
+    ).toEqual({
+      ...initialState,
+      addItem: REQUEST_STATE.FULFILLED,
+      list: [...initialState.list, itemToAdd],
+    });
+  });
+
+  it('should handle addItemAsync.rejected', () => {
+    const error = new Error('Error adding item');
+    const rejectedAction = addItemAsync.rejected(error);;
+    expect(
+      itemsReducer(initialState, rejectedAction)
+    ).toEqual({
+      ...initialState,
+      addItem: REQUEST_STATE.REJECTED,
+      error: rejectedAction.error,
+    });
+  });
+
+  it('should handle deleteItemAsync.pending', () => {
+    expect(
+      itemsReducer(initialState, deleteItemAsync.pending())
+    ).toEqual({
+      ...initialState,
+      deleteItem: REQUEST_STATE.PENDING,
+      error: null,
+    });
+  });
+
+  it('should handle deleteItemAsync.fulfilled', () => {
+    const itemIdToDelete = 'item-id-1';
+    const stateWithItems = {
+      ...initialState,
+      list: [
+        { _id: 'item-id-1', name: 'Item 1', description: 'Description 1', price: 10 },
+        { _id: 'item-id-2', name: 'Item 2', description: 'Description 2', price: 20 },
+      ],
+    };
+    expect(
+      itemsReducer(stateWithItems, deleteItemAsync.fulfilled(itemIdToDelete))
+    ).toEqual({
+      ...initialState,
+      deleteItem: REQUEST_STATE.FULFILLED,
+      list: [{ _id: 'item-id-2', name: 'Item 2', description: 'Description 2', price: 20 }],
+    });
+  });
+
+  it('should handle deleteItemAsync.rejected', () => {
+    const error = new Error('Error deleting item');
+    const rejectedAction = deleteItemAsync.rejected(error);
+    expect(
+      itemsReducer(initialState, rejectedAction)
+    ).toEqual({
+      ...initialState,
+      deleteItem: REQUEST_STATE.REJECTED,
+      error: rejectedAction.error, // Updated to use the error object from the action
+    });
+  });
+
+  it('should handle addUnitAsync.pending', () => {
+    expect(
+      itemsReducer(initialState, addUnitAsync.pending())
+    ).toEqual({
+      ...initialState,
+      addUnit: REQUEST_STATE.PENDING,
+      error: null,
+    });
+  });
+
+  it('should handle addUnitAsync.fulfilled', () => {
+    const itemId = 'item-id-1';
+    const unitsToAdd = 1;
+    const stateWithItems = {
+      ...initialState,
+      list: [
+        { _id: 'item-id-1', name: 'Item 1', description: 'Description 1', price: 10, unitsRemaining: 10 },
+        { _id: 'item-id-2', name: 'Item 2', description: 'Description 2', price: 20, unitsRemaining: 15 },
+      ],
+    };
+    expect(
+      itemsReducer(stateWithItems, addUnitAsync.fulfilled({ itemId, unitsToAdd }))
+    ).toEqual({
+      ...initialState,
+      addUnit: REQUEST_STATE.FULFILLED,
+      list: [
+        { _id: 'item-id-1', name: 'Item 1', description: 'Description 1', price: 10, unitsRemaining: 11 },
+        { _id: 'item-id-2', name: 'Item 2', description: 'Description 2', price: 20, unitsRemaining: 15 },
+      ],
+    });
+  });
+
+  it('should handle addUnitAsync.rejected', () => {
+    const error = new Error('Error adding units');
+    const rejectedAction = addUnitAsync.rejected(error);
+    expect(
+      itemsReducer(initialState, rejectedAction)
+    ).toEqual({
+      ...initialState,
+      addUnit: REQUEST_STATE.REJECTED,
+      error: rejectedAction.error, // Updated to use the error object from the action
+    });
+  });
+
+});
